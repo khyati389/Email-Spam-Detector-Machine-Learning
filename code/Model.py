6dc3dbf6345bd871815a4eccc2dc022cbc5317f9
khyati389 <khyatikhyati389@gmail.com>
2020-04-25 13:59:43 -0400

    Removed unnecessary import

--- code/Model.py
+++ code/Model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 
 '''
--- code/main.py
+++ code/main.py
@@ -1,8 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import sys
-import os
-import re
 from Processor import TextProcessor
 from Processor import FileProcessor
 
